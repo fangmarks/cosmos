062464e359f673a02dbd4f9f42bfae48c5dd73f7
Joshua <lio@werewolf.design>
2022-04-17 15:30:37 +0200

    add opengraph data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,31 +3,34 @@ import Domains from "components/Domains";
 import Header from "components/Header";
 import { sort } from "utils/sort";
 import { Domain, Filter as EFilter } from "utils/types";
-import Filter, { Context } from "components/Filter";
+import { Context } from "components/Filter";
 import { useContext } from "react";
+import { Helmet } from "react-helmet";
 
 export async function getServerSideProps() {
   let { items: domains, count } = await db.fetch();
   return { props: { domains, count } };
 }
 
-/**
- * TODO: Add Ability to filter based on Tags / services mutuals
- * TODO: can ask to have an account on/with
- */
-
 const Index = ({ domains, count }) => {
   // @ts-ignore
   const [filter] = useContext(Context);
-  
+
   let d = sort(domains as Domain[], filter);
   // let d = domains
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>lio.domains</title>
+        <meta property="og:title" content="lio.domains" />
+        <meta property="og:description" content="lio.domains is a List of all Domains hosted/operated by @Himbolion" />
+        <meta property="og:image" content="/Nebula.webp" />
+      </Helmet>
       <div style={{ textAlign: "center", margin: "auto" }}>
-          <Header />
-          <Domains domains={d} count={count} />
+        <Header />
+        <Domains domains={d} count={count} />
       </div>
     </>
   );
